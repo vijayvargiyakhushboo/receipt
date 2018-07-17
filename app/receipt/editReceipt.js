05b305131ee5790fcb8d59b4713d0056b7821ac3
khushboo <khushboovjvrg018@gmail.com>
2018-07-17 18:36:38 +0530

    update receipt

--- app/receipt/editReceipt.js
+++ app/receipt/editReceipt.js
@@ -3,20 +3,36 @@ reciept.controller('editReceiptCtrl', function($rootScope,$scope,$routeParams,RE
 	$scope.class = CLASS;
 	$scope.types = TYPES;
 	$scope.section = SECTION;
-	console.log("route id :"+($scope.receiptId));
+	$scope.receipt = {studentName: '', date : '', class: '', section: '', feeType: '', amount: '',deleted: 0};
 
 	$scope.getReceipt = (id)=> {
-    q.selectAllById(RECEIPT_TABLE,'id',$scope.receiptId)
-    .then((rows)=>
-      $timeout(()=>{
-        $scope.receipt = rows[0];
-        $scope.receipt.date = $scope.receipt.date ? new Date($scope.receipt.date) : undefined;
-      },0)
-      )};
+		q.selectAllById(RECEIPT_TABLE,'id',$scope.receiptId)
+		.then((rows)=>
+			$timeout(()=>{
+				$scope.receipt = rows[0];
+				$scope.receipt.date = $scope.receipt.date ? new Date($scope.receipt.date) : undefined;
+			},0)
+			)};
 
-    $scope.getReceipt($scope.receiptId);
-	
-})
+		$scope.submitReceipt = (receipt)=>{
+			let keys = Object.keys($scope.receipt);
+			let values = Object.values($scope.receipt);
+			q.update(RECEIPT_TABLE,keys,values,'id',$scope.receiptId)
+			.then((data)=>{
+				$timeout(()=>{
+					$scope.resetReceipt();
+				},0);
+			})
+			.catch((err)=>{
+				console.error('err , receipt updation', err);
+			});
+		};
+
+		$scope.resetReceipt =()=>{
+			$scope.receipt= {};
+		};
+		$scope.getReceipt($scope.receiptId);
+	})
 .constant('TYPES', ['Admission Fee' , 'Tution Fee', 'Exam Fee', 'Other Fee'])
 .constant('SECTION', ['A','B','C'])
 .constant('CLASS', ['Nursery','LKG','UKG','1','2','3','4','5','6','7','8','9','10','11','12']);
