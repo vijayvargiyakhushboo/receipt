3102ebbc18de1e35a1bfe6aaf594c1447953a615
khushboo <khushboovjvrg018@gmail.com>
2018-07-17 15:26:11 +0530

    update student

--- app/student/editStudent.js
+++ app/student/editStudent.js
@@ -1,16 +1,37 @@
-reciept.controller('editStudentCtrl',  function($rootScope,$scope,$timeout,STUDENT_TABLE,$routeParams){
-$scope.studentId = $routeParams.id;
-console.log("editStudent.js");
-	/*$scope.student = {name:'',fatherName:'', motherName:'',dob:'',address:'',gender:'',phone:'',email:'',className:'',};
+reciept.controller('editStudentCtrl',  function($rootScope,$scope,$timeout,STUDENT_TABLE,$routeParams,GENDER,CLASS){
+  $scope.studentId = $routeParams.id;
+  $scope.student = {name:'',fatherName:'', motherName:'',dob:'',address:'',gender:'',phone:'',email:'',className:'',deleted:0};
+  $scope.gender = GENDER;
+  $scope.className = CLASS;
 
-	$scope.init = ()=> {
-      q.selectAllById(STUDENT_TABLE, 'id', $scope.studentId)
-      .then((rows)=>
-        $timeout(()=> {
+  $scope.resetStudent = ()=> {
+    $scope.student= {};
+  }
+
+  $scope.getStudent = (id)=> {
+    q.selectAllById(STUDENT_TABLE,'id',$scope.studentId)
+    .then((rows)=>
+      $timeout(()=>{
         $scope.student = rows[0];
         $scope.student.dob = $scope.student.dob ? new Date($scope.student.dob) : undefined;
       },0)
-    )};
+      )};
 
-	 $scope.init(); */
-});
+    $scope.submitStudent =(student)=> {
+      let keys = Object.keys($scope.student);
+      let values = Object.values($scope.student);
+      q.update(STUDENT_TABLE, keys, values,'id',$scope.studentId)
+      .then((data)=>{
+        $timeout(()=>{
+          $scope.resetStudent();
+        },0);
+
+      })
+      .catch((err)=>{
+        console.error('err, student updation', err);
+      });
+    };
+
+    $scope.getStudent($scope.studentId);
+  }).constant('GENDER', ['Female','Male'])
+.constant('CLASS', ['Nursery','LKG','UKG','1','2','3','4','5','6','7','8','9','10','11','12']);
