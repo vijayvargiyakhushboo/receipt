c53726396f25907c28febc83916f0b57bd1d404c
khushboo <khushboovjvrg018@gmail.com>
2018-08-08 15:40:09 +0530

    add receipt

--- app/receipt/receipt.js
+++ app/receipt/receipt.js
@@ -2,55 +2,63 @@ reciept.controller('receiptCtrl',  function($rootScope,$scope,$timeout,TYPES,REC
 	$scope.types = TYPES;
 	$scope.class = CLASS;
 	$scope.receipt = {studentName: '', date : '', class: '', admissionFee: '',tutionFee:'',examFee:'',otherFee:'',deleted: 0};
-    $scope.student={};
-	$scope.resetReceipt = ()=>{
-		$scope.receipt ={};
-	};
+  $scope.student={};
+  $scope.resetReceipt = ()=>{
+    $scope.receipt ={};
+    $scope.receiptForm.$setPristine();
+    $scope.receiptForm.$setUntouched();
+  };
 
-	$scope.submitReceipt =(receipt)=> {
-		console.log("receipt :",receipt);
-		let keys = Object.keys($scope.receipt);
-		let values = Object.values($scope.receipt);
-		console.log("submitReceipt");
-		q.insert(RECEIPT_TABLE, keys, values)
-		.then((data)=>{
-			$timeout(()=>{
-				$scope.resetReceipt();
-			},0);
-		})
-		.catch((err)=>{
-			console.error('err, receipt insertion', err);
-		});
-	};
+  $scope.submitReceipt =(receipt)=> {
+    let keys = Object.keys($scope.receipt);
+    let values = Object.values($scope.receipt);
+    q.insert(RECEIPT_TABLE, keys, values)
+    .then((data)=>{
+     $timeout(()=>{
+      $scope.resetReceipt();
+    },0);
+   })
+    .catch((err)=>{
+     console.error('err, receipt insertion', err);
+   });
+  };
 
-	$scope.searchStudent = (keyword)=>{
-		console.log("searchStudent ",$scope.student);
-      let result = [];
-      let student = [];
-      console.log(".. ",$scope.student);
-      for(let st of $scope.student){
-        st.name.toLowerCase().indexOf(keyword.toLowerCase()) > -1 ? result.push(st) : student.push(st);
-      }
-      return result.length > 0 ? result :student;
-    };
+  $scope.searchStudent = (keyword)=>{
+    let result = [];
+    let student = [];
+    for(let st of $scope.student){
+      st.name.toLowerCase().indexOf(keyword.toLowerCase()) > -1 ? result.push(st) : student.push(st);
+    }
+    $scope.receipt.studentName = result[0].id;
+    return result.length > 0 ? result :student;
+  };
 
-    $scope.getDataByTable = (tableName, modelName)=>{
-    	q.selectAll(tableName)
-    	.then((rows)=>{
-    		if(rows)
-    			for(let row of rows){
-    				row.dob = row.date ? new Date(row.dob) : null;
-    			}
-    			$scope[modelName] = rows;
-    			console.log("KV ",$scope.student);
-    			
-    		})
-    	.catch((err)=>{
-    		console.error(err);
-    	});
-    };
-     $scope.getDataByTable(STUDENT_TABLE, STUDENT_TABLE);
+  $scope.updateSelectedStud = (student)=>{
+    if(student && student.id){
+      $scope.student = student;
+    }else{
+      $scope.student = null;
+      $scope.receipt = [];
+    }
+  };
+
+  $scope.getDataByTable = (tableName, modelName)=>{
+   q.selectAll(tableName)
+   .then((rows)=>{
+    if(rows)
+     for(let row of rows){
+      row.dob = row.date ? new Date(row.dob) : null;
+    }
+    $scope[modelName] = rows;
+
+  })
+   .catch((err)=>{
+    console.error(err);
+  });
+ };
+ $scope.getDataByTable(STUDENT_TABLE, STUDENT_TABLE);
 
 })
-.constant('TYPES', ['Admission Fee' , 'Tution Fee', 'Exam Fee', 'Other Fee'])
+//.constant('TYPES', ['Admission Fee' , 'Tution Fee', 'Exam Fee', 'Other Fee'])
+.constant('TYPES', ['admissionFee' , 'tutionFee', 'examFee', 'otherFee'])
 .constant('CLASS', ['Nursery','LKG','UKG','1','2','3','4','5','6','7','8','9','10','11','12']);
